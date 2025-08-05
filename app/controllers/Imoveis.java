52bb6ed01caeb8315b1c9de0305cc159efe0925c
Natan Paulo <natanpaulo1300@gmail.com>
2025-08-05 17:42:47 -0300

    Vericando por código

--- app/controllers/Imoveis.java
+++ app/controllers/Imoveis.java
@@ -1,11 +1,55 @@
 package controllers;
 
+import java.util.List;
+
+import models.Imovel;
+import models.Status;
+import models.TipoImovel;
 import play.mvc.Controller;
 
 public class Imoveis extends Controller{
 	
-	public static void form() {
-		render();
+	public static void formimovel() {
+		List<TipoImovel> tipos = TipoImovel.find("status <> ?1", Status.DESATIVADO).fetch();
+		render(tipos);
 	}
 	
+	public static void salvar(Imovel imovel) {
+		List<Imovel> imoveis = Imovel.find("status <> ?1", Status.DESATIVADO).fetch();
+		if (imovel != null) {
+			
+			for(Imovel i : imoveis) {
+				if(i.codigoAnuncio.equals(imovel.codigoAnuncio)) {
+					flash.error("Ocorreu algum erro ao salvar");
+					listar();
+				}
+			}
+			
+			imovel.save();
+			flash.success("Imóvel salvo com sucesso!");
+			detalhar(imovel.id);
+		} else {
+			flash.error("Ocorreu algum erro ao salvar");
+			listar();
+		}
+		
+	}
+	
+	public static void listar() {
+		List<Imovel> imoveis = Imovel.find("status <> ?1", Status.DESATIVADO).fetch();
+		render(imoveis);
+	}
+	
+	
+	public static void detalhar(Long id) {
+		Imovel imovel = Imovel.findById(id);
+		render(imovel);
+	}
+	
+	public static void editar(Long id) {
+		List<TipoImovel> tipos = TipoImovel.find("status <> ?1", Status.DESATIVADO).fetch();
+		Imovel imovel = TipoImovel.findById(id);
+		renderTemplate("Imoveis/formimovel.html", imovel, tipos);
+		
+	}
 }
--- app/models/Imovel.java
+++ app/models/Imovel.java
@@ -1,6 +1,8 @@
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.ManyToOne;
 
 import play.db.jpa.Model;
@@ -10,13 +12,26 @@ public class Imovel extends Model{
 	public Integer codigoAnuncio;
 	public String bairro;
 	public Integer quantidadeComodos;
+	@Override
+	public String toString() {
+		return "Imovel: codigoAnuncio=" + codigoAnuncio + ", bairro=" + bairro + ", quantidadeComodos="
+				+ quantidadeComodos + ", areaInterna=" + areaInterna + ", areaExterna=" + areaExterna + ", tipoImovel=" + tipoImovel.descricao;
+	}
+
 	public Double areaInterna;
 	public Double areaExterna;
+	
+	@Enumerated(EnumType.STRING)
+	public Status status;
 	
 	@ManyToOne
 	public TipoImovel tipoImovel;
 	
 	public Double getAreaTotal() {
 		return this.areaInterna + this.areaExterna;
+	}
+	
+	public Imovel() {
+		this.status = Status.ATIVO;
 	}
 }
